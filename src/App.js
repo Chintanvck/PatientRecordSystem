342ea51398b6ed990588defa4b8ed83fc5329aad
“20it058 <20it058@charusat.edu.in>
2022-09-09 13:15:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './Components/NavigationBar';
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PatientList from './Components/PatientList';
+import AddPatient from './Components/AddPatient';
+import UpdatePatient from './Components/UpdatePatient';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <NavigationBar />
+      <Container>
+          <Routes>
+            <Route path="addpatient" element={<AddPatient />} />
+            <Route
+              path="editpatient/:patientId"
+              element={<UpdatePatient  />}
+            />
+            <Route
+              path="listpatient"
+              element={<PatientList />}
+            />
+          </Routes>
+        </Container>
+      </Router>
     </div>
   );
 }
